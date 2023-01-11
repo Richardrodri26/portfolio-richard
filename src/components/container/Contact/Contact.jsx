295cf5770877f190736f1bcd1c0703d6ead9299e
Richardrodri26 <richardmanuel811@gmail.com>
2023-01-11 14:52:21 -0500

    first commit

---
+++ src/components/container/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import "./Contact.scss";
+import { motion } from 'framer-motion';
+
+const Contact = () => {
+  return (
+    <div className="container" id="contact">
+      <motion.div
+        initial={{opacity: 0}}
+        whileInView={{y: [-50, 0], opacity: 1}} 
+        className="title"
+      >
+            <span>get in touch</span>
+            <h1>Contact Me</h1>
+      </motion.div>
+
+      <motion.div
+        initial={{opacity: 0}}
+        whileInView={{y: [-50, 0], opacity: 1}} 
+        className="title"
+      >
+      <a target="_blank" href="https://docdro.id/dHHYPqg">Check my CV</a>
+      </motion.div>
+    
+
+    </div>
+  )
+}
+
+export default Contact
